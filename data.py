2dbba2d2763f5ff7b62f9ceae1a69ad757150681
Laurent Abbal <laurent@abbal.com>
2025-11-19 20:55:59 +0900

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+employees = [
+    {'experience': 5, 'education': 3, 'gender': 'F', 'salary': 2400},
+    {'experience': 3, 'education': 3, 'gender': 'M', 'salary': 2550},
+    {'experience': 5, 'education': 5, 'gender': 'F', 'salary': 2500},
+    {'experience': 3, 'education': 5, 'gender': 'M', 'salary': 2800},
+    {'experience': 2, 'education': 5, 'gender': 'F', 'salary': 2300},
+    {'experience': 2, 'education': 3, 'gender': 'M', 'salary': 2700}
+]
